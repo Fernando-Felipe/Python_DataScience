8c7aa04be49e745bbd3ee117849f6e56564c4378
Fernando Gonçalves <fernandogonfs@hotmail.com>
2025-10-13 14:06:23 -0300

    teste2

---
+++ teste2.py
@@ -0,0 +1,11 @@
+duvida = 'Quem veio antes? O ovo? Ou foi a serpente?'
+lista_palavras = duvida.split('?')
+print(lista_palavras)
+
+misturas = ['Tintas: vermelho, azul e amarelo',
+            'Verde: mistura de azul e amarelo',
+            'Laranja: mistura de vermelho e amarelo',
+            'Roxo: mistura de vermelho e azul']
+unificador = '. '
+string_misturas = unificador.join(misturas)
+print(string_misturas)
